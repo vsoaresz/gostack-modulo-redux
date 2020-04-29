839adafe0a8c4d809e69b141e8c58613f9888369
Vitor Soares <vitor@Vitors-MacBook-Pro.local>
2020-04-29 00:02:22 -0300

    add add_cart_reducer

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
 import { Container, Cart } from "./styles";
@@ -7,20 +8,27 @@ import { MdShoppingBasket } from "react-icons/md";
 
 import logo from "../../assets/images/logo.svg";
 
-export default function Header() {
-  return (
-    <Container>
-      <Link to="/">
-        <img src={logo} alt="Rocketshoes" />
-      </Link>
+class Header extends Component {
+  render() {
+    const { cart } = this.props;
+    return (
+      <Container>
+        <Link to="/">
+          <img src={logo} alt="Rocketshoes" />
+        </Link>
 
-      <Cart to="/cart">
-        <div>
-          <strong>Meu carrinho</strong>
-          <span>3 itens</span>
-        </div>
-        <MdShoppingBasket size={36} color="#FFF" />
-      </Cart>
-    </Container>
-  );
+        <Cart to="/cart">
+          <div>
+            <strong>Meu carrinho</strong>
+            <span>{cart.length} itens</span>
+          </div>
+          <MdShoppingBasket size={36} color="#FFF" />
+        </Cart>
+      </Container>
+    );
+  }
 }
+
+export default connect((state) => ({
+  cart: state.cart,
+}))(Header);
